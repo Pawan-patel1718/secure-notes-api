cd77ee32266942e5a567b85e57eb96affd147c0e
Pawan Patel <pp895006@gmail.com>
2023-10-01 10:52:17 +0530

    database configuration change

--- db_con.js
+++ db_con.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql2")
+require('dotenv').config();
+
+// console.log(process.env.DB_HOST)
+// console.log(process.env.DB_USER_NAME)
+// console.log(process.env.DB_PASS)
+// console.log(process.env.DB_NAME)
+
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "secure_notes"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER_NAME,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 })
 
 module.exports = connection;
